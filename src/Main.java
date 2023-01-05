59e385e482691598cad1919b55690a879b146f13
TamanaMansoori <106633241+TamanaMansoori@users.noreply.github.com>
2023-01-04 20:39:14 -0600

    versoion 4

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello syntax");
         System.out.println("Hello Tamana");
+        System.out.println("Hello all");
     }
 }
